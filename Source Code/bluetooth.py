79c482530e66ba002a757b4642f9284a9053db57
Jordan Rood <jrood2024@nevada.unr.edu>
2023-11-24 17:32:15 -0800

    TvData added with tshark analysis modifications

--- Source Code/bluetooth.py
+++ Source Code/bluetooth.py
@@ -1,5 +1,6 @@
 import bluetooth
 
+# discover nearby devices (i.e., connected devices)
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
 print("Found " + str(len(nearby_devices)) + " devices!")
 
@@ -13,9 +14,11 @@ socketConn = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 socketConn.bind(("", bluetooth.PORT_ANY))
 socketConn.listen(1)
 
+# socket connection accept command
 client_socket, client_address = server_socket.accept()
 print(f"Accepted connection from {client_address}")
 data_received = client_socket.recv(1024)
 
+# close both the client and server socket safely
 client_socket.close()
 server_socket.close()
--- Source Code/tsharkAnalysis.py
+++ Source Code/tsharkAnalysis.py
@@ -4,7 +4,8 @@
 # import package to run system calls
 import os
 
-dataDir = "../Data"
+# directory variables
+dataDir = "../TvData"
 dataAnalysisDir = "../Data-Analysis"
 
 # if directory already exists, remove directory for reanalysis
@@ -17,17 +18,18 @@ os.mkdir(dataAnalysisDir)
 # loop through tshark calls for analysis and export of data to Data Analysis directory - calls for all data collections
 for x in range(1, 11):
     # construct input and output filename
-    infilename = "collection-" + str(x) + ".pcapng"
+    infilename = "tv-collection-" + str(x) + ".pcapng"
     outfilename = "analysis-" + str(x) + ".csv"
 
     print("dataDirPath ", os.path.join(dataDir, infilename))
 
-    # construct tshark command
-    cmd = "tshark -r " + os.path.join(dataDir, infilename) + " -T fields -e ip.src -e ip.dst -e _ws.col.Protocol -E header=y -E separator=,  > " + os.path.join(dataAnalysisDir, outfilename)
+    # construct tshark command for fields and filtered by tv's IP address
+    cmd = "tshark -r " + os.path.join(dataDir, infilename) + " -T fields -e ip.src -e ip.dst -e _ws.col.Protocol -e _ws.col.Length -e tcp.port -Y \"ip.src==192.168.1.124 or ip.dst==192.168.1.124\" -E header=y -E separator=,  > " + os.path.join(dataAnalysisDir, outfilename)
     
     print(cmd)
-    # make command call
+    # make tshark command call
     os.system(cmd)
+
 
 # Example tshark call
 # "tshark -r ../Data/Iphone-Data-Capture.pcapng -T fields -e ip.src -e ip.dst -e _ws.col.Protocol -E header=y -E separator=,  > ../Data-Analysis/test.xlsx"
